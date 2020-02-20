ce91e14abf20ef2edab1ecd9bfdeee4f56efd056
devwenzel <jose.wenzel@codurance.com>
2020-02-20 16:29:27 +0000

    add test for NIC

--- test/generator.test.ts
+++ test/generator.test.ts
@@ -20,4 +20,20 @@ describe('the salary generator', () => {
         );
     });
 
+    it('should print for 9060 annual gross', () => {
+
+        employee = {
+            employeeId: 12345,
+            employeeName: 'John J Doe',
+            grossSalary: 9060
+        };
+
+        expect(generateSlipSalary(employee)).toBe(
+            'Employee ID: 12345\n' +
+            'Employee Name: John J Doe\n' +
+            'Gross Salary: £416.67\n' +
+            'National Insurance contributions: £10.00'
+        );
+    });
+
 });
